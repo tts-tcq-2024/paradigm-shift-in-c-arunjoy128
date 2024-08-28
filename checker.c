b1821b7f07d2d4cc7853318d5ebbe1cf25c8a58b
arunjoy128 <arunabrahamjoy@gmail.com>
2024-08-28 17:19:01 +0530

    Update checker.c
    To reduce ccn

--- checker.c
+++ checker.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
 #include <assert.h>
 
+int temperatureIsOk(float temperature) 
+{
+return (temperature>=0&&temperature<=45);
+}
+int socIsOk(float soc)
+{
+return (soc>=20 && soc<=80);
+}
+int chargeRateIsOk(float chargeRate)
+{
+return (chargeRate<=0.8);
+}
+
+int batteryIsOk(float temperature, float soc, float chargeRate)
+{
+  if(!temperatureIsOk(temperature))
+  {
+  printf("Temperature out of range!\n");
+  return 0;
+  }
+  if(!socIsOk(float soc))
+  {
+  printf("State of Charge out of range!\n");
+  return 0;
+  }
+  if(!chargeRateIsOk(float chargeRate)
+  {
+  printf("Charge Rate out of range!\n");
+  return 0;
+  }
+return 1;
+}
+
+int main() 
+{
+  assert(batteryIsOk(25, 70, 0.7));
+  assert(!batteryIsOk(50, 85, 0));
+}
+
+/*
 int batteryIsOk(float temperature, float soc, float chargeRate) {
   if(temperature < 0 || temperature > 45) {
     printf("Temperature out of range!\n");
@@ -16,3 +56,4 @@ int main() {
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
 }
+*/
